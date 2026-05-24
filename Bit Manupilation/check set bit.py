b82474711c597f92287cea7779047ee033ec23b8
Ruthvik2000 <setty.ruthvik2109@gmail.com>
2026-05-24 19:41:59 +0530

    Create check set bit.py

---
+++ Bit Manupilation/check set bit.py
@@ -0,0 +1,8 @@
+"""
+Whenever we calculate the binary number of an integer value then it is formed as the combination of 0's and 1's. 
+So, the digit 1 is known as set bit in the terms of the computer
+"""
+#Set bits in a binary number is represented by 1
+"""
+Setting a bit means that if K-th bit in the binary number is 0, then set it to 1 and if it is 1 then leave it unchanged
+"""
